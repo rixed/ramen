a2225a9a79917d12d12d666bab1726c79f64c5fa
Cedric Cellier <rixed@happyleptic.org>
2017-05-27 07:08:50 -0600

    Use a single alarm for all escalations
    Makes reloading of persist data just works.
    Also simplify escalation/alert datastructures.

--- src/ramen/Alerter.ml
+++ src/ramen/Alerter.ml
@@ -36,9 +36,10 @@ type escalation_step =
     victims : int array (* Who to ring from the oncallers squad *) }
 
 type escalation =
-  { steps : escalation_step array ;
-    mutable acknowledged : bool ;
-    alarm : Alarm.t }
+  { alert : alert ; incident : incident ;
+    steps : escalation_step array ;
+    mutable attempt : int ;
+    mutable last_sent : float }
 
 (* The part of the internal state that we persist on disk: *)
 type persisted_state = {
@@ -49,14 +50,17 @@ type persisted_state = {
    * we do not need this hash since we already have all escalation in
    * alarms, but we'd like to save this from time to time on disk to help
    * not loosing track of what's happening in case of crash: *)
-  mutable ongoing_escalations : (escalation, int * float) Hashtbl.t ;
+  mutable ongoing_escalations : (int, escalation) Hashtbl.t ;
 
   (* All global (aka team-wide) inhibits, keyed by team name, value being
    * expiration of the inhibit *)
   team_inhibits : (string, float) Hashtbl.t ;
 
   (* All inhibited alerts, keyed by alert name*team *)
-  alert_inhibits : (string * string, float) Hashtbl.t }
+  alert_inhibits : (string * string, float) Hashtbl.t ;
+
+  (* Used to give a unique id to any escalation so that we can ack them. *)
+  mutable next_alert_id : int }
 
 type state = {
   (* Where this state is saved *)
@@ -72,6 +76,13 @@ type state = {
   stmt_get_contacts : Sqlite3.stmt ;
   stmt_get_escalation : Sqlite3.stmt ;
 
+  (* The alarm we use for all escalation ; It's simpler than having one
+   * alarm per escalation especially since it avois having to persist
+   * alarm closures. *)
+  alarm : Alarm.t ;
+
+  (* What is saved onto/restored from disk to avoid losing context when
+   * stopping or crashing: *)
   persist : persisted_state }
 
 let is_inhibited state name team =
@@ -130,6 +141,8 @@ struct
   type alert_outcome =
     Duplicate | Inhibited | STFU | Notify of Contact.t
   type log_event = NewAlert of (alert * incident * alert_outcome)
+                 (* TODO: we'd like to have the origin of this ack. *)
+                 | AckUnknown of int | Ack of escalation
   let add = ignore (* TODO *)
 end
 
@@ -170,8 +183,9 @@ end
 
 module Sender =
 struct
-  let printer ~title ~text =
-    Printf.printf "Title: %s\n\n%s\n%!" title text
+  let printer id alert =
+    Printf.printf "Title: %s\nId: %d\n%s\n%!"
+      alert.title id alert.text
 
   let get = function
     | Contact.Console -> printer
@@ -181,7 +195,7 @@ end
 
 module Escalation =
 struct
-  let default_escalation () =
+  let default_escalation alert incident =
     (* First send type 0, then immediately send type 1 then
      * after 5 minutes send type 2 then after 10 minutes send type 3,
      * and repeat type 3 every 10 minutes: *)
@@ -191,9 +205,9 @@ struct
         { timeout = 600. ; victims = [| 0 ; 1 |] } ;
         { timeout = 600. ; victims = [| 0 ; 1 |] }
       |] ;
-      acknowledged = false ; alarm = Alarm.make () }
+      alert ; incident ; attempt = 0 ; last_sent = !Alarm.now }
 
-  let rec make state team importance =
+  let make state alert incident =
     let to_array mask =
       let rec loop prev bit =
         if bit >= 64 then Array.of_list prev else
@@ -201,30 +215,36 @@ struct
           if 0L = Int64.(logand mask (shift_left 1L bit)) then prev else
           bit :: prev in
         loop prev' (bit+1) in
-      loop [] 0
+      loop [] 0 in
+    (* We might not find a configuration corresponding to this alert
+     * importance. In that case we take the configuration for the next
+     * most important alerts: *)
+    let rec loop importance =
+      if importance < 0 then ( (* end the recursion *)
+        default_escalation alert incident
+      ) else (
+        let open Sqlite3 in
+        let stmt = state.stmt_get_escalation in
+        reset stmt |> must_be_ok ;
+        bind stmt 1 Data.(TEXT alert.team) |> must_be_ok ;
+        bind stmt 2 Data.(INT (Int64.of_int importance)) |> must_be_ok ;
+        let steps =
+          step_all_fold stmt [] (fun prev ->
+            let timeout = column stmt 1 |> to_float |> required
+            and victims = column stmt 2 |> to_int64 |> required |> to_array
+            in
+            { timeout ; victims } :: prev) |>
+          List.rev |>
+          Array.of_list in
+        if steps = [||] then loop (importance - 1) else
+        { steps ; alert ; incident ;
+          attempt = 0 ; last_sent = !Alarm.now }
+      )
     in
-    if importance < 0 then ( (* end the recursion *)
-      default_escalation ()
-    ) else (
-      let open Sqlite3 in
-      let stmt = state.stmt_get_escalation in
-      reset stmt |> must_be_ok ;
-      bind stmt 1 Data.(TEXT team) |> must_be_ok ;
-      bind stmt 2 Data.(INT (Int64.of_int importance)) |> must_be_ok ;
-      let steps =
-        step_all_fold stmt [] (fun prev ->
-          let timeout = column stmt 1 |> to_float |> required
-          and victims = column stmt 2 |> to_int64 |> required |> to_array
-          in
-          { timeout ; victims } :: prev) |>
-        List.rev |>
-        Array.of_list in
-      if steps = [||] then make state team (importance - 1) else
-      { steps ; acknowledged = false ; alarm = Alarm.make () }
-    )
+    loop alert.importance
 
   (* Send that alert using message king for this step *)
-  let send_alert state attempt alert incident victims =
+  let send_alert state id esc victims =
     (* We figure out who the oncaller are at each attempt of each alert of an
      * incident. This means that if the incident happends during an oncall
      * shift the new oncallers get involved in the middle of the action while
@@ -238,29 +258,24 @@ struct
      * so we now need to know who exactly are we going to bother: *)
     Array.iter (fun rank ->
         let open OnCaller in
-        let victim = who_is_oncall state alert.team rank !Alarm.now in
-        let contact = get_cap victim.contacts attempt in
-        let send = Sender.get contact in
-        Log.(add (NewAlert (alert, incident, Notify contact))) ;
-        send ~title:alert.title ~text:alert.text
+        let victim = who_is_oncall state esc.alert.team rank !Alarm.now in
+        let contact = get_cap victim.contacts esc.attempt in
+        let sender = Sender.get contact in
+        Log.(add (NewAlert (esc.alert, esc.incident, Notify contact))) ;
+        sender id esc.alert
       ) victims
 
   (* Send a message and prepare the escalation *)
-  let rec send state e attempt alert incident =
-    let step = get_cap e.steps attempt in
-    send_alert state attempt alert incident step.victims ;
-    Alarm.at e.alarm (!Alarm.now +. step.timeout) (fun () ->
-      if not e.acknowledged then (
-        Hashtbl.modify e (fun (step, _) -> step+1, !Alarm.now)
-                       state.persist.ongoing_escalations ;
-        state.dirty <- true ;
-        send state e (attempt + 1) alert incident
-      ))
+  let send state id esc =
+    let step = get_cap esc.steps esc.attempt in
+    send_alert state id esc step.victims
 
-  let start state e alert incident =
-    Hashtbl.add state.persist.ongoing_escalations e (0, !Alarm.now) ;
+  let start state esc =
+    let id = state.persist.next_alert_id in
+    state.persist.next_alert_id <- state.persist.next_alert_id + 1 ;
+    Hashtbl.add state.persist.ongoing_escalations id esc ;
     state.dirty <- true ;
-    send state e 0 alert incident
+    send state id esc
 end
 
 let open_config_db file =
@@ -311,6 +326,16 @@ let open_config_db file =
     db_close db |> must_be string_of_bool true ;
     db_open ~mode:`READONLY file
 
+let check_escalations state () =
+  Hashtbl.iter (fun id esc ->
+      let step = get_cap esc.steps esc.attempt in
+      if !Alarm.now >= esc.last_sent +. step.timeout then (
+        (* We must escalate *)
+        esc.attempt <- esc.attempt + 1 ;
+        esc.last_sent <- !Alarm.now ;
+        Escalation.send state id esc)
+    ) state.persist.ongoing_escalations
+
 let get_state save_file db_config_file =
   let persist =
     try
@@ -321,34 +346,38 @@ let get_state save_file db_config_file =
       { ongoing_incidents = [] ;
         ongoing_escalations = Hashtbl.create 11 ;
         team_inhibits = Hashtbl.create 11 ;
-        alert_inhibits = Hashtbl.create 11 }
+        alert_inhibits = Hashtbl.create 11 ;
+        next_alert_id = 0 }
     in
-  let db = open_config_db db_config_file in
-  { save_file ; dirty = false ; db ;
-    stmt_get_oncall =
-      Sqlite3.prepare db
-        "SELECT oncallers.name \
-         FROM schedule NATURAL JOIN oncallers \
-         WHERE schedule.rank <= ? AND schedule.from <= ? \
-           AND oncallers.team = ? \
-         ORDER BY schedule.from DESC, schedule.rank DESC LIMIT 1" ;
-    stmt_get_contacts =
-      Sqlite3.prepare db
-        "SELECT contact FROM contacts \
-         WHERE oncaller = ? ORDER BY preferred DESC" ;
-    stmt_get_escalation =
-      Sqlite3.prepare db
-        "SELECT timeout, victims FROM escalations \
-         WHERE team = ? AND importance = ? ORDER BY attempt ASC" ;
-    persist }
-
+  let alarm = Alarm.make ()
+  and db = open_config_db db_config_file in
+  let state =
+    { save_file ; dirty = false ; db ; alarm ;
+      stmt_get_oncall =
+        Sqlite3.prepare db
+          "SELECT oncallers.name \
+           FROM schedule NATURAL JOIN oncallers \
+           WHERE schedule.rank <= ? AND schedule.from <= ? \
+             AND oncallers.team = ? \
+           ORDER BY schedule.from DESC, schedule.rank DESC LIMIT 1" ;
+      stmt_get_contacts =
+        Sqlite3.prepare db
+          "SELECT contact FROM contacts \
+           WHERE oncaller = ? ORDER BY preferred DESC" ;
+      stmt_get_escalation =
+        Sqlite3.prepare db
+          "SELECT timeout, victims FROM escalations \
+           WHERE team = ? AND importance = ? ORDER BY attempt ASC" ;
+      persist } in
+  Alarm.every 1. (check_escalations state) ;
+  state
 
 let save_state state =
   if state.dirty then (
     if debug then
       Printf.eprintf "Saving state in file %S\n%!" state.save_file ;
     File.with_file_out ~mode:[`create; `trunc] state.save_file (fun oc ->
-      Marshal.output oc ~sharing:false state) ;
+      Marshal.output oc state) ;
     state.dirty <- false
   )
 
@@ -365,7 +394,16 @@ let alert state ~name ~team ~importance ~title ~text =
   ) else if incident.stfu then (
     Log.(add (NewAlert (alert, incident, STFU))) ;
   ) else (
-    let esc = Escalation.make state team importance in
-    Escalation.start state esc alert incident ;
+    let esc = Escalation.make state alert incident in
+    Escalation.start state esc ;
     save_state state
   )
+
+let acknowledge state id =
+  match Hashtbl.find state.ongoing_escalations id with
+  | exception Not_found ->
+    Printf.eprintf "Received an ack for unknown id %d\n" id ;
+    Log.(add (AckUnknown id))
+  | esc ->
+    Log.(add (Ack esc)) ;
+    Hashtbl.remove state.ongoing_escalations id
