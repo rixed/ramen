168dc25ae822069d521e95c3a0e40a52ce29b6dc
Cedric Cellier <rixed@happyleptic.org>
2017-06-16 08:47:58 +0200

    Fix tests broken after recent change in Expr type.

--- src/rigatoni/Lang.ml
+++ src/rigatoni/Lang.ml
@@ -179,8 +179,10 @@ let same_tuple_as_in = function
   open Lang.P
 
   let test_printer res_printer = function
-    | Ok (res, (len, _)) ->
-      Printf.sprintf "%S, parsed_len=%d" (IO.to_string res_printer res) len
+    | Ok (res, (len, rest)) ->
+      Printf.sprintf "%S, parsed_len=%d, rest=%s"
+        (IO.to_string res_printer res) len
+        (IO.to_string (List.print Char.print) rest)
     | Bad (Approximation _) ->
       "Approximation"
     | Bad (NoSolution e) ->
@@ -191,6 +193,55 @@ let same_tuple_as_in = function
   let test_p p s =
     (p +- eof) [] None Parsers.no_error_correction (stream_of_string s) |>
     to_result
+
+  let typ = Lang.Expr.make_typ "replaced for tests"
+
+  let rec replace_typ =
+    let open Lang.Expr in
+    function
+    | Const (_, a) -> Const (typ, a)
+    | Field (_, a, b) -> Field (typ, a, b)
+    | Param (_, a) -> Param (typ, a)
+    | AggrMin (_, a) -> AggrMin (typ, replace_typ a)
+    | AggrMax (_, a) -> AggrMax (typ, replace_typ a)
+    | AggrSum (_, a) -> AggrSum (typ, replace_typ a)
+    | AggrAnd (_, a) -> AggrAnd (typ, replace_typ a)
+    | AggrOr  (_, a) -> AggrOr  (typ, replace_typ a)
+    | AggrPercentile (_, a, b) -> AggrPercentile (typ, replace_typ a, replace_typ b)
+    | Age (_, a) -> Age (typ, replace_typ a)
+    | Not (_, a) -> Not (typ, replace_typ a)
+    | Defined (_, a) -> Defined (typ, replace_typ a)
+    | Add (_, a, b) -> Add (typ, replace_typ a, replace_typ b)
+    | Sub (_, a, b) -> Sub (typ, replace_typ a, replace_typ b)
+    | Mul (_, a, b) -> Mul (typ, replace_typ a, replace_typ b)
+    | Div (_, a, b) -> Div (typ, replace_typ a, replace_typ b)
+    | Exp (_, a, b) -> Exp (typ, replace_typ a, replace_typ b)
+    | And (_, a, b) -> And (typ, replace_typ a, replace_typ b)
+    | Or (_, a, b) -> Or (typ, replace_typ a, replace_typ b)
+    | Ge (_, a, b) -> Ge (typ, replace_typ a, replace_typ b)
+    | Gt (_, a, b) -> Gt (typ, replace_typ a, replace_typ b)
+    | Eq (_, a, b) -> Eq (typ, replace_typ a, replace_typ b)
+
+  let replace_typ_in_expr = function
+    | Ok (expr, rest) -> Ok (replace_typ expr, rest)
+    | x -> x
+
+  let replace_typ_in_op =
+    let open Lang.Operation in
+    function
+    | Ok (Select { fields ; and_all_others ; where }, rest) ->
+      Ok (Select {
+        fields = List.map (fun sf -> { sf with expr = replace_typ sf.expr }) fields ;
+        and_all_others ; where = replace_typ where }, rest)
+    | Ok (Aggregate { fields ; and_all_others ; where ; key ; emit_when }, rest) ->
+      Ok (Aggregate {
+        fields = List.map (fun sf -> { sf with expr = replace_typ sf.expr }) fields ;
+        and_all_others ;
+        where = replace_typ where ;
+        key = replace_typ key ;
+        emit_when = replace_typ emit_when }, rest)
+    | Ok (OnChange e, rest) -> Ok (OnChange (replace_typ e), rest)
+    | x -> x
  *)
 
 module Scalar =
@@ -391,14 +442,15 @@ struct
     typ.nullable <> None && typ.typ <> None
 
   let print_typ fmt typ =
-    Printf.fprintf fmt "%s%s"
+    Printf.fprintf fmt "%s%s of %s"
       (match typ.nullable with
       | None -> "maybe nullable "
       | Some true -> "nullable "
       | Some false -> "")
+      typ.name
       (match typ.typ with
-      | None -> "unknwon type"
-      | Some typ -> IO.to_string Scalar.print_typ typ)
+      | None -> "unknown type"
+      | Some typ -> "type "^ IO.to_string Scalar.print_typ typ)
 
   let make_typ ?nullable ?typ name = { name ; nullable ; typ }
   let make_bool_typ ?nullable name = make_typ ?nullable ~typ:Scalar.TBool name
@@ -414,7 +466,7 @@ struct
     (* Valid only within an aggregation operation; but must be here to allow
      * operations on top of the result of an aggregation function, such as: "(1
      * + min field1) / (max field2)". Even within an aggregation, not valid
-     * within another AggrFun. *)
+     * within another aggregation function. *)
     | AggrMin of typ * t
     | AggrMax of typ * t
     | AggrSum of typ * t
@@ -481,7 +533,8 @@ struct
       Scalar.Parser.p >>: fun c ->
       Const (make_typ ~nullable:false ~typ:(Scalar.type_of c) "constant", c)
     (*$= const & ~printer:(test_printer print)
-      (Ok (Const (Scalar.VBool true), (4, [])))  (test_p const "true")
+      (Ok (Const (typ, Scalar.VBool true), (4, [])))\
+        (test_p const "true" |> replace_typ_in_expr)
     *)
 
     let field =
@@ -492,20 +545,20 @@ struct
       Field (make_typ (tuple ^"."^ field), tuple, field)
     (*$= field & ~printer:(test_printer print)
       (Ok (\
-        Field { tuple="in"; field="bytes" },\
+        Field (typ, "in", "bytes"),\
         (5, [])))\
-        (test_p field "bytes")
+        (test_p field "bytes" |> replace_typ_in_expr)
 
       (Ok (\
-        Field { tuple="in"; field="bytes" },\
+        Field (typ, "in", "bytes"),\
         (8, [])))\
-        (test_p field "in.bytes")
+        (test_p field "in.bytes" |> replace_typ_in_expr)
 
       (Bad (\
         NoSolution (\
           Some { where = ParsersMisc.Item (7, '.');\
                  what=["eof"]})))\
-        (test_p field "pasglop.bytes")
+        (test_p field "pasglop.bytes" |> replace_typ_in_expr)
     *)
 
     let param =
@@ -513,15 +566,15 @@ struct
       Param (make_typ ("parameter "^s), s)
     (*$= param & ~printer:(test_printer print)
       (Ok (\
-        Param "glop",\
+        Param (typ, "glop"),\
         (5, [])))\
-        (test_p param "$glop")
+        (test_p param "$glop" |> replace_typ_in_expr)
 
       (Bad (\
         NoSolution (\
           Some { where = ParsersMisc.Item (0, 'g');\
                  what = ["\"$\""] })))\
-      (test_p param "glop")
+      (test_p param "glop" |> replace_typ_in_expr)
     *)
 
     (* operators with lowest precedence *)
@@ -611,69 +664,69 @@ struct
 
     (*$= p & ~printer:(test_printer print)
       (Ok (\
-        Const (Scalar.VBool true),\
+        Const (typ, Scalar.VBool true),\
         (4, [])))\
-        (test_p p "true")
+        (test_p p "true" |> replace_typ_in_expr)
 
       (Ok (\
-        Not (Defined (Field {tuple="in";field="zone_src"})),\
+        Not (typ, Defined (typ, Field (typ, "in", "zone_src"))),\
         (16, [])))\
-        (test_p p "zone_src IS NULL")
+        (test_p p "zone_src IS NULL" |> replace_typ_in_expr)
 
       (Ok (\
-        Eq (Field {tuple="in";field="zone_src"}, Param "z1"),\
+        Eq (typ, Field (typ, "in", "zone_src"), Param (typ, "z1")),\
         (14, [])))\
-        (test_p p "zone_src = $z1")
+        (test_p p "zone_src = $z1" |> replace_typ_in_expr)
 
       (Ok (\
-        And (\
-          Or (\
-            Not (Defined (Field {tuple="in";field="zone_src"})),\
-            Eq (Field {tuple="in";field="zone_src"}, Param "z1")),\
-          Or (\
-            Not (Defined (Field {tuple="in";field="zone_dst"})),\
-            Eq (Field {tuple="in";field="zone_dst"}, Param "z2"))),\
+        And (typ, \
+          Or (typ, \
+            Not (typ, Defined (typ, Field (typ, "in", "zone_src"))),\
+            Eq (typ, Field (typ, "in", "zone_src"), Param (typ, "z1"))),\
+          Or (typ, \
+            Not (typ, Defined (typ, Field (typ, "in", "zone_dst"))),\
+            Eq (typ, Field (typ, "in", "zone_dst"), Param (typ, "z2")))),\
         (77, [])))\
-        (test_p p "(zone_src IS NULL or zone_src = $z1) AND \\
-                   (zone_dst IS NULL or zone_dst = $z2)")
+        (test_p p "(zone_src IS NULL or zone_src = $z1) and \\
+                   (zone_dst IS NULL or zone_dst = $z2)" |> replace_typ_in_expr)
 
       (Ok (\
-        Div (\
-          AggrFun (AggrSum (Field {tuple="in"; field="bytes"})),\
-          Param "avg_window"),\
+        Div (typ, \
+          AggrSum (typ, Field (typ, "in", "bytes")),\
+          Param (typ, "avg_window")),\
         (23, [])))\
-        (test_p p "(sum bytes)/$avg_window")
+        (test_p p "(sum bytes)/$avg_window" |> replace_typ_in_expr)
 
       (Ok (\
-        Div (\
-          Field {tuple="in";field="start"},\
-          Mul (\
-            Const (Scalar.VI32 1_000_000l),\
-            Param "avg_window")),\
+        Div (typ, \
+          Field (typ, "in", "start"),\
+          Mul (typ, \
+            Const (typ, Scalar.VI32 1_000_000l),\
+            Param (typ, "avg_window"))),\
         (33, [])))\
-        (test_p p "start / (1_000_000 * $avg_window)")
+        (test_p p "start / (1_000_000 * $avg_window)" |> replace_typ_in_expr)
 
       (Ok (\
-        AggrFun (AggrPercentile (\
-          Param "p",\
-          Field { tuple="in"; field="bytes_per_sec" })),\
+        AggrPercentile (typ,\
+          Param (typ, "p"),\
+          Field (typ, "in", "bytes_per_sec")),\
         (30, [])))\
-        (test_p p "$p percentile of bytes_per_sec")
+        (test_p p "$p percentile of bytes_per_sec" |> replace_typ_in_expr)
 
       (Ok (\
-        Gt (\
-          AggrFun (AggrMax (\
-            Field { tuple="others"; field="start" })),\
-          Add (\
-            Field { tuple="out"; field="start" },\
-            Mul (\
-              Mul (\
-                Param "obs_window",\
-                Const (Scalar.VFloat 1.15)),\
-              Const (Scalar.VI32 1_000_000l)))),\
+        Gt (typ, \
+          AggrMax (typ, \
+            Field (typ, "others", "start")),\
+          Add (typ, \
+            Field (typ, "out", "start"),\
+            Mul (typ, \
+              Mul (typ, \
+                Param (typ, "obs_window"),\
+                Const (typ, Scalar.VFloat 1.15)),\
+              Const (typ, Scalar.VI32 1_000_000l)))),\
         (63, [])))\
         (test_p p "max others.start > \\
-                   out.start + ($obs_window * 1.15) * 1_000_000")
+                   out.start + ($obs_window * 1.15) * 1_000_000" |> replace_typ_in_expr)
     *)
 
     (*$>*)
@@ -842,70 +895,70 @@ struct
       (Ok (\
         Select {\
           fields = [\
-            { expr = Expr.(Field { tuple = "in" ; field = "start" }) ;\
+            { expr = Expr.(Field (typ, "in", "start")) ;\
               alias = [ "start" ] } ;\
-            { expr = Expr.(Field { tuple = "in" ; field = "stop" }) ;\
+            { expr = Expr.(Field (typ, "in", "stop")) ;\
               alias = [ "stop" ] } ;\
-            { expr = Expr.(Field { tuple = "in" ; field = "itf_clt" }) ;\
+            { expr = Expr.(Field (typ, "in", "itf_clt")) ;\
               alias = [ "itf_src" ] } ;\
-            { expr = Expr.(Field { tuple = "in" ; field = "itf_srv" }) ;\
+            { expr = Expr.(Field (typ, "in", "itf_srv")) ;\
               alias = [ "itf_dst" ] } ] ;\
           and_all_others = false ;\
-          where = Expr.Const (Scalar.VBool true) },\
+          where = Expr.Const (typ, Scalar.VBool true) },\
         (58, [])))\
-        (test_p p "select start, stop, itf_clt as itf_src, itf_srv as itf_dst")
+        (test_p p "select start, stop, itf_clt as itf_src, itf_srv as itf_dst" |>\
+         replace_typ_in_op)
 
       (Ok (\
         Select {\
           fields = [] ;\
           and_all_others = true ;\
           where = Expr.(\
-            Gt (\
-              Field { tuple="in" ; field="packets" },\
-              Const (Scalar.VI8 (Int8.of_int 0)))) },\
+            Gt (typ,\
+              Field (typ, "in", "packets"),\
+              Const (typ, Scalar.VI8 (Int8.of_int 0)))) },\
         (17, [])))\
-        (test_p p "where packets > 0")
+        (test_p p "where packets > 0" |> replace_typ_in_op)
 
       (Ok (\
         Aggregate {\
           fields = [\
-            { expr = Expr.(AggrFun (\
-                AggrMin (Field { tuple = "in" ; field = "start" }))) ;\
+            { expr = Expr.(\
+                AggrMin (typ, Field (typ, "in", "start"))) ;\
               alias = [ "start"; "out_start" ] } ;\
-            { expr = Expr.(AggrFun (\
-                AggrMax (Field { tuple = "in" ; field = "stop" }))) ;\
+            { expr = Expr.(\
+                AggrMax (typ, Field (typ, "in", "stop"))) ;\
               alias = [] } ;\
             { expr = Expr.(\
-                Div (\
-                  AggrFun (\
-                    AggrSum (Field { tuple = "in" ; field = "packets" })),\
-                  Param "avg_window")) ;\
+                Div (typ,\
+                  AggrSum (typ,Field (typ, "in", "packets")),\
+                  Param (typ, "avg_window"))) ;\
               alias = [ "packets_per_sec" ] } ] ;\
           and_all_others = false ;\
-          where = Expr.Const (Scalar.VBool true) ;\
+          where = Expr.Const (typ, Scalar.VBool true) ;\
           key = Expr.(\
-            Div (\
-              Field { tuple = "in" ; field = "start" },\
-              Mul (\
-                Const (Scalar.VI32 1_000_000l),\
-                Param "avg_window"))) ;\
+            Div (typ,\
+              Field (typ, "in", "start"),\
+              Mul (typ,\
+                Const (typ, Scalar.VI32 1_000_000l),\
+                Param (typ, "avg_window")))) ;\
           emit_when = Expr.(\
-            Gt (\
-              Add (\
-                AggrFun (\
-                  AggrMax (Field { tuple = "any" ; field = "start" })),\
-                Const (Scalar.VI16 (Int16.of_int 3600))),\
-              Field { tuple = "out" ; field = "start" })) },\
+            Gt (typ,\
+              Add (typ,\
+                AggrMax (typ,Field (typ, "any", "start")),\
+                Const (typ, Scalar.VI16 (Int16.of_int 3600))),\
+              Field (typ, "out", "start"))) },\
           (183, [])))\
           (test_p p "select min start as start or out_start, max stop, \\
                             (sum packets)/$avg_window as packets_per_sec \\
                      group by start / (1_000_000 * $avg_window) \\
-                     emit after out.start < (max any.start) + 3600")
+                     emit after out.start < (max any.start) + 3600" |>\
+           replace_typ_in_op)
 
       (Ok (\
-        OnChange Expr.(Field { tuple = "in" ; field = "too_low" }),\
+        OnChange Expr.(Field (typ, "in", "too_low")),\
         (17, [])))\
-        (test_p p "on change too_low")
+        (test_p p "on change too_low" |> replace_typ_in_op)
 
       (Ok (\
         Alert { team = "network firefighters" ;\
