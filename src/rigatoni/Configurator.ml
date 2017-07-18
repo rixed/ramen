7a6224dfa6dc1d7c318e9ad5c51d2ce7208f5996
Cedric Cellier <rixed@happyleptic.org>
2017-07-18 11:30:47 +0200

    ramen_configurator: fix flushing condition

--- src/rigatoni/Configurator.ml
+++ src/rigatoni/Configurator.ml
@@ -165,7 +165,7 @@ let graph_info_of_bcns csv_dir bcns =
           "SELECT %gth percentile of bytes_per_secs AS bps,\n\
                   zone_src, zone_dst\n\
            GROUP BY min_capture_begin // %d\n\
-           COMMIT AND SLIDE 1 WHEN in.#count > %d"
+           COMMIT AND SLIDE 1 WHEN in.#count >= %d"
            bcn.percentile
            obs_window
            (Helpers.round_to_int (bcn.obs_window /. bcn.avg_window)) in
