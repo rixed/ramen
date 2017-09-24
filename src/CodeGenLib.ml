f1f2efae822e0867459a6d66d8bd96c37cfd28e9
Cedric Cellier <rixed@happyleptic.org>
2017-09-24 20:43:50 +0200

    Fix bloom filter false positive ratio
    It was 1.5% not 15% (although 15% would be better eventually)

--- src/CodeGenLib.ml
+++ src/CodeGenLib.ml
@@ -73,7 +73,7 @@ type remember_state =
     mutable last_remembered : bool }
 
 let remember_init tim dur e =
-  let nb_slices = 10 and false_positive_ratio = 0.15 in
+  let nb_slices = 10 and false_positive_ratio = 0.015 in
   let start_time = tim -. dur and slice_width = dur /. float_of_int nb_slices in
   let filter = RamenBloomFilter.make_sliced start_time nb_slices slice_width false_positive_ratio in
   let last_remembered = RamenBloomFilter.remember filter tim e in
