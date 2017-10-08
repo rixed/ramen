45ae5e4571ea8112bf88364914547fa88d159882
Cedric Cellier <rixed@happyleptic.org>
2017-10-08 21:34:28 +0200

    Fix: still crashes, use simpler interface with GC

--- src/wrap_collectd.c
+++ src/wrap_collectd.c
@@ -23,11 +23,11 @@ static void set_nullable_string(value block, unsigned idx, char const *str)
   CAMLparam1(block);
   CAMLlocal1(tmp);
   if (!str || str[0] == '\0')
-    caml_modify(&Field(block, idx), Val_int(0));
+    Store_field(block, idx, Val_int(0));
   else {
     tmp = caml_alloc(1, 0);
-    caml_modify(&Field(tmp, 0), caml_copy_string(str));
-    caml_modify(&Field(block, idx), tmp);
+    Store_field(tmp, 0, caml_copy_string(str));
+    Store_field(block, idx, tmp);
   }
   CAMLreturn0;
 }
@@ -55,23 +55,23 @@ CAMLprim value wrap_collectd_decode(value buffer_, value nb_bytes_)
     struct collectd_metric *m = metrics + i;
     assert(m->nb_values > 0);
     m_tup = caml_alloc(6 + COLLECTD_NB_VALUES, 0);
-    caml_modify(&Field(m_tup, 0), caml_copy_string(m->host));
-    caml_modify(&Field(m_tup, 1), caml_copy_double(m->time));
+    Store_field(m_tup, 0, caml_copy_string(m->host));
+    Store_field(m_tup, 1, caml_copy_double(m->time));
     set_nullable_string(m_tup, 2, m->plugin_name);
     set_nullable_string(m_tup, 3, m->plugin_instance);
     set_nullable_string(m_tup, 4, m->type_name);
     set_nullable_string(m_tup, 5, m->type_instance);
-    caml_modify(&Field(m_tup, 6+0), caml_copy_double(m->values[0]));
+    Store_field(m_tup, 6+0, caml_copy_double(m->values[0]));
     unsigned v;
     for (v = 1; v < m->nb_values; v++) {
       tmp = caml_alloc(1, 0);
-      caml_modify(&Field(tmp, 0), caml_copy_double(m->values[v]));
-      caml_modify(&Field(m_tup, 6+v), tmp);
+      Store_field(tmp, 0, caml_copy_double(m->values[v]));
+      Store_field(m_tup, 6+v, tmp);
     }
     for (; v < COLLECTD_NB_VALUES; v++) {
-      caml_modify(&Field(m_tup, 6+v), Val_int(0)); // None
+      Store_field(m_tup, 6+v, Val_int(0)); // None
     }
-    caml_modify(&Field(res, i), m_tup);
+    Store_field(res, i, m_tup);
   }
 
   switch (status) {
