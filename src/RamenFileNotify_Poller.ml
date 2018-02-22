f1a15c038306f0b40863ef46c25dd9747c15abb6
Cedric Cellier <rixed@happyleptic.org>
2018-02-22 11:56:07 +0100

    Fix possible memory leak in RamenFileNotify_Poller
    Not 100% sure but likelly causing memory leak in #134.

--- src/RamenFileNotify_Poller.ml
+++ src/RamenFileNotify_Poller.ml
@@ -14,54 +14,57 @@ let rec for_each f n =
   if not (n.while_ ()) then return_unit else
   let%lwt files = Lwt_unix.files_of_directory n.dirname |>
                   Lwt_stream.to_list in
-  let files = Array.of_list files in (* FIXME *)
-  (*!logger.debug "%d files in %s" (Array.length files) n.dirname ;*)
-  Array.fast_sort String.compare files ;
-  let rec merge prev i next_reported =
+  let files = List.fast_sort String.compare files in
+  let nb_files = List.length files in
+  !logger.info "%d files to monitor." nb_files ;
+  (* Merge the list of files with the previously known files (in [n.reported]).
+   * Both are ordered alphabetically. The result is then stored in
+   * [n.reported]. *)
+  let rec merge prev next files =
     if not (n.while_ ()) then (
       !logger.info "Stop listening to directory %s" n.dirname ;
       return_unit
-    ) else if i >= Array.length files then (
-      (*!logger.debug "No new files in %s" n.dirname ;*)
-      n.reported <- List.rev_append prev next_reported ;
-      let%lwt () = Lwt_unix.sleep 1. in
-      for_each f n
-    ) else (
-      match Unix.stat (n.dirname ^"/"^ files.(i)) with
-      | exception Unix.Unix_error (Unix.ENOENT, _, _) ->
-          (* File might have been read and deleted by another worker already *)
-          merge prev (i + 1) next_reported
-      | s ->
-        if s.Unix.st_kind = Unix.S_DIR then
-          merge prev (i + 1) next_reported
-        else (
-          let f_mtime = s.Unix.st_mtime in
-          match next_reported with
-          | (r, r_mtime as rpair)::rest ->
-            (match String.compare r files.(i) with
-            | 0 ->
-              if f_mtime > r_mtime then  (
-                !logger.debug "File %S has changed" files.(i) ;
-                let%lwt () = f files.(i) in
-                merge ((r, f_mtime) :: prev) (i + 1) rest
-              ) else (
-                (* Still the same, keep going *)
-                merge (rpair :: prev) (i + 1) rest
-              )
-            | 1 ->
-              (* files.(i) is new: insert and notify *)
-              !logger.debug "File %S is new" files.(i) ;
-              let%lwt () = f files.(i) in
-              merge ((files.(i), f_mtime) :: prev) (i + 1) next_reported
-            | _ ->
-              !logger.debug "File %S is new" files.(i) ;
-              let%lwt () = f files.(i) in
-              merge ((files.(i), f_mtime) :: rpair :: prev) (i + 1) rest
+    ) else match files with
+    | [] ->
+        List.iter (fun (r, _) -> !logger.debug "File %S is gone" r) next ;
+        n.reported <- List.rev prev ;
+        let%lwt () = Lwt_unix.sleep 1. in
+        for_each f n
+    | (file :: files') as files ->
+        (match%lwt Lwt_unix.stat (n.dirname ^"/"^ file) with
+        | exception Unix.Unix_error (Unix.ENOENT, _, _) ->
+            (* File might have been read and deleted by another worker already *)
+            merge prev next files'
+        | s ->
+            if s.Unix.st_kind = Unix.S_DIR then
+              merge prev next files'
+            else (
+              let f_mtime = s.Unix.st_mtime in
+              match next with
+              | (r, r_mtime as rpair)::rest ->
+                let cmp = String.compare r file in
+                if cmp = 0 then (
+                  if f_mtime > r_mtime then  (
+                    !logger.debug "File %S has changed" file ;
+                    let%lwt () = f file in
+                    merge ((r, f_mtime) :: prev) rest files'
+                  ) else (
+                    (* Still the same, keep going *)
+                    merge (rpair :: prev) rest files'
+                  )
+                ) else if cmp > 0 then (
+                  (* file is new: insert and notify *)
+                  !logger.debug "File %S is new" file ;
+                  let%lwt () = f file in
+                  merge ((file, f_mtime) :: prev) next files'
+                ) else (
+                  !logger.debug "File %S is gone" r ;
+                  merge prev rest files
+                )
+              | [] ->
+                !logger.debug "File %S is new" file ;
+                let%lwt () = f file in
+                merge ((file, f_mtime) :: prev) [] files'
             )
-          | [] ->
-            !logger.debug "File %S is new" files.(i) ;
-            let%lwt () = f files.(i) in
-            merge ((files.(i), f_mtime) :: prev) (i + 1) []
-        )
-    ) in
-  merge [] 0 n.reported
+        ) in
+  merge [] n.reported files
