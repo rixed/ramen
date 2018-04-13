f8fcf77bfa0dc6618423890874a4e250c44ec5a7
Cedric Cellier <rixed@happyleptic.org>
2018-04-13 08:38:28 +0200

    Make all stdout/err messages end up in log files

--- src/RamenLog.ml
+++ src/RamenLog.ml
@@ -22,19 +22,26 @@ let log_file tm =
     (tm.Unix.tm_year+1900) (tm.Unix.tm_mon+1) tm.Unix.tm_mday
 
 let output =
-  let fd = ref None and fname = ref "" in
+  let ocr = ref None and fname = ref "" in
   fun ?logdir tm is_err ->
     match logdir with
     | Some logdir ->
       let fname' = log_file tm in
       if fname' <> !fname then (
+        let open Legacy.Unix in
         fname := fname' ;
-        Option.may IO.close_out !fd ;
+        Option.may (ignore_exceptions @@ close_out) !ocr ;
         let path = logdir ^"/"^ !fname in
-        fd := Some (File.open_out ~mode:[`append;`create;`text] path)
+        let fd = BatUnix.openfile path [O_WRONLY; O_APPEND; O_CREAT; O_CLOEXEC] 0o644 in
+        (* Make sure everything that gets written to stdout/err end up
+         * logged in that file too: *)
+        dup2 fd stderr ;
+        dup2 fd stdout ;
+        let oc = BatUnix.out_channel_of_descr fd in
+        ocr := Some oc
       ) ;
-      Option.get !fd
-    | None -> if is_err then IO.stderr else IO.stdout
+      Option.get !ocr
+    | None -> if is_err then stderr else stdout
 
 let make_logger ?logdir ?(prefix="") dbg =
   let prefix = colored "1;34" prefix in
@@ -57,4 +64,3 @@ let make_logger ?logdir ?(prefix="") dbg =
   { error ; warning ; info ; debug ; logdir }
 
 let logger = ref (make_logger false)
-
