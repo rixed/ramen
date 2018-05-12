5e11556bb77190ac428cbdcb4ec85cb925134685
Cedric Cellier <rixed@happyleptic.org>
2018-05-12 20:02:14 +0200

    Allow to ommit the `1` in a duration expression.

--- src/RamenParsing.ml
+++ src/RamenParsing.ml
@@ -70,7 +70,7 @@ let number =
 let duration m =
   let m = "duration" :: m in
   let strinGs s = strinG s ||| strinG (s ^"s") in
-  (number +- blanks ++
+  (optional ~def:1. (number +- blanks) ++
    ((strinGs "microsecond" >>: fun () -> 0.000_001) |||
     (strinGs "millisecond" >>: fun () -> 0.001) |||
     (strinGs "second" >>: fun () -> 1.) |||
