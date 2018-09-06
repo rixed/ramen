08337a208ddb2de371ff2c521f00af3325a20953
Cedric Cellier <rixed@happyleptic.org>
2018-09-06 22:44:53 +0200

    Bump version to 3.0.4

--- src/RamenVersions.ml
+++ src/RamenVersions.ml
@@ -3,7 +3,7 @@
 
 (* Release tag just for information purpose but not actually used to version
  * anything. *)
-let release_tag = "v3.0.3"
+let release_tag = "v3.0.4"
 
 (* Format of the list of running programs: *)
 let graph_config = "v10" (* last: params in RC key *)
