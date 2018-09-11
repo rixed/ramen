5c2f78e905fa367ec02227bc0aa3ba74a0ee0a96
Cedric Cellier <rixed@happyleptic.org>
2018-09-11 09:03:22 +0200

    Fix inability to open former ringbufs

--- src/RamenVersions.ml
+++ src/RamenVersions.ml
@@ -21,7 +21,7 @@ let notify_tuple = "v6" (* last: firing is now optional *)
 let pending_notify = "v4" (* last: max FPR *)
 
 (* Ringbuf formats *)
-let ringbuf = "v4" (* last: nullable in RamenTypes.t *)
+let ringbuf = "v5" (* last: Cache-aware alignment *)
 
 (* Ref-ringbuf format *)
 let out_ref = "v3" (* last: use PPP *)
