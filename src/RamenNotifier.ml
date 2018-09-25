03ea8d6f25a7fa00d0061c3ed7fe9d148037f2a8
Cedric Cellier <rixed@happyleptic.org>
2018-09-25 19:22:03 +0200

    notifier: Fix quoting in default conf

--- src/RamenNotifier.ml
+++ src/RamenNotifier.ml
@@ -608,8 +608,8 @@ let ensure_conf_file_exists notif_conf_file =
           -X POST \
           -H 'Content-Type: application/json' \
           --data-raw '[{\"labels\":{\
-               \"alertname\":\"${name}\",\
-               \"summary\":\"${text}\",\
+               \"alertname\":\"'${name}'\",\
+               \"summary\":\"'${text}'\",\
                \"severity\":\"critical\"}}]' \
           'http://localhost:9093/api/v1/alerts'" in
     { teams =
