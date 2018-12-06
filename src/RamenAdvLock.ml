a6dc19f6436d16e74f71fc4e3b8ba33442f8331c
Cedric Cellier <rixed@happyleptic.org>
2018-12-06 14:43:15 +0100

    Allow several threads to AdvLock the same file
    Will deadlock if the same thread attempt to advlock the same file though.

--- src/RamenAdvLock.ml
+++ src/RamenAdvLock.ml
@@ -4,28 +4,37 @@ open RamenLog
 open RamenHelpers
 open RamenAtomic
 
-(* TODO: some kind of error when we recurse would be great *)
-
-let write_locked = ref BatSet.String.empty
+let write_locked = Hashtbl.create 11
 
 let with_lock op fname f =
-  assert (not (BatSet.String.mem fname !write_locked)) ;
-  if op = F_LOCK then
-    write_locked := BatSet.String.add fname !write_locked ;
-  mkdir_all ~is_file:true fname ;
-  let fd = openfile fname [O_RDWR; O_CLOEXEC; O_CREAT] 0o640 in
-  finally
-    (fun () ->
-      if op = F_LOCK then
-        write_locked := BatSet.String.remove fname !write_locked ;
-      close fd)
-    (fun () ->
+  let mutex =
+    if op = F_LOCK then Some (
+      match Hashtbl.find write_locked fname with
+      | exception Not_found ->
+          let mutex = Mutex.create () in
+          Mutex.lock mutex ;
+          Hashtbl.add write_locked fname mutex ;
+          mutex
+      | mutex ->
+          if not (Mutex.try_lock mutex) then (
+            !logger.warning "File %S is already locked, waiting!"
+              fname ;
+            Mutex.lock mutex (* Wait for that other thread... *)) ;
+          mutex
+    ) else None in
+  finally (fun () -> Option.may Mutex.unlock mutex)
+          (fun () ->
+    mkdir_all ~is_file:true fname ;
+    let fd = openfile fname [O_RDWR; O_CLOEXEC; O_CREAT] 0o640 in
+    finally (fun () -> close fd) (fun () ->
       lockf fd op 0 ;
       finally
         (fun () ->
           lseek fd 0 SEEK_SET |> ignore ;
           lockf fd F_ULOCK 0)
-        f fd) ()
+        f fd
+    ) ()
+  ) ()
 
 let with_r_lock fname = with_lock F_RLOCK fname
 let with_w_lock fname = with_lock F_LOCK fname
