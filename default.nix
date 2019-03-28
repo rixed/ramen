5f13192b00f5c9c3c5caad73e27bac613ed71921
Cedric Cellier <rixed@happyleptic.org>
2019-03-28 10:03:59 +0100

    Nix config: WIP

--- default.nix
+++ default.nix
@@ -1,22 +1,30 @@
-{ stdenv, fetchFromGitHub, ocaml, findlib, batteries, stdint, syslog, lacaml,
+{ stdenv, darwin, ocaml, findlib, batteries, stdint, syslog, lacaml,
   cmdliner, ocaml_sqlite3, qtest, inotify, binocle, net_codecs,
-  parsercombinator, pretty-printers-parsers, z3 }:
+  parsercombinator, pretty-printers-parsers, z3,
+  liborc, cyrus_sasl, openssl, blas, liblapack }:
 
 stdenv.mkDerivation rec {
   pname = "ramen";
   version = "3.3.0";
 
-  src = fetchFromGitHub {
-    owner = "rixed";
-    repo = "ramen";
-    rev = "v${version}";
-    sha256 = "17mx2fqsp8xlhx118rm2607fncyf91n27szw648akhlxr2il6n9z";
+  src = builtins.fetchGit {
+    url = "/home/rixed/share/src/ramen";
+    #"https://github.com/rixed/ramen.git";
+    ref = "nobundle";
   };
 
   buildInputs = [
-    ocaml findlib batteries stdint syslog lacaml cmdliner ocaml_sqlite3
-    qtest binocle net_codecs parsercombinator pretty-printers-parsers
+    ocaml findlib batteries syslog stdint lacaml cmdliner
+    ocaml_sqlite3 qtest binocle net_codecs parsercombinator
+    pretty-printers-parsers liborc
   ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [ inotify ];
+
+  configureFlags = [
+    "--with-liborc=${liborc.out}/lib"
+    ("--with-runtime-libdirs=${cyrus_sasl.out}/lib,${openssl.out}/lib,${blas.out}/lib,${liblapack.out}/lib" +
+     (if stdenv.isDarwin then ",${darwin.apple_sdk.frameworks.Accelerate}" else ""))
+    "--with-runtime-incdirs=${liborc.out}/include"
+  ];
 
   createFindlibDestdir = true;
 
