f895091aa9e37879ed2902ed5aa925c6fa8e38c8
Cedric Cellier <rixed@happyleptic.org>
2020-02-18 05:21:40 +0100

    Fix not replacing "${*|...}" in templates

--- src/RamenStringExpansion.ml
+++ src/RamenStringExpansion.ml
@@ -14,7 +14,7 @@ open RamenHelpers
 let subst_dict =
   let open Str in
   let re =
-    regexp "\\${\\([_a-zA-Z][-_a-zA-Z0-9|?:, ]*\\|\\*\\)}" in
+    regexp "\\${\\([_a-zA-Z*][-_a-zA-Z0-9|?:, ]*\\)}" in
   fun dict ?(quote=identity) ?null text ->
     let to_value var_name =
       try List.assoc var_name dict
@@ -92,8 +92,12 @@ let subst_dict =
   "glop"          (subst_dict ["f", " \tglop  "] "${f|trim}")
   "{\"a\":\"1\",\"b\":\"2\"}" \
                   (subst_dict ["a", "1"; "b", "2"] "${a,b|json}")
+  "{\"a\":\"1\",\"b\":\"2\"}" \
+                  (subst_dict ["a", "1"; "b", "2"] "${*|json}")
   "{\"a\":\"pas\",\"b\":\"glop\"}" \
                   (subst_dict ["a", " pas "; "b", " \tglop "] "${a,b|trim|json}")
+  "{\"a\":\"pas\",\"b\":\"glop\"}" \
+                  (subst_dict ["a", " pas "; "b", " \tglop "] "${*|trim|json}")
   "1.2,2.4"       (subst_dict ["a", "1.2"; "b", "2.4"] "${a,b}")
   "1,2"           (subst_dict ["a", "1.2"; "b", "2.4"] "${a,b|int}")
  *)
