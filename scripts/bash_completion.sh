ba64edb665f1c5abd2c7a31974a72567b150dfc9
Damien Bihel <dbihel@accedian.com>
2020-02-26 14:59:10 +0100

    completion: check $PATH when we complete ramen.

--- scripts/bash_completion.sh
+++ scripts/bash_completion.sh
@@ -2,7 +2,9 @@
 
 _ramen_completions()
 {
-  COMPREPLY=($(compgen -W "$(${COMP_WORDS[0]} _completion " ${COMP_WORDS[*]:1}" | sed 's/\t/ /' | cut -d ' ' -f 1)"))
+  if [ -x "$(command -v ${COMP_WORDS[0]})" ] ; then
+    COMPREPLY=($(compgen -W "$(${COMP_WORDS[0]} _completion " ${COMP_WORDS[*]:1}" | sed 's/\t/ /' | cut -d ' ' -f 1)"))
+  fi
 }
 
 complete -F _ramen_completions ramen
