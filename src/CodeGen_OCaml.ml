rixed/ramen

commit dfce37536cc49a90e6e64774cf7103a9fa955319

Fix `Get` from lists or vectors when index is non const
In that case the result is always nullable (when reading a non existing
element).

src/CodeGen_OCaml.ml
@@ -1304,12 +1304,22 @@ and emit_expr_ ~env ~context ~opc oc expr =
         ~impl_return_nullable:ts.(n).nullable
         [None, PropagateNull] oc [e]
     in
+    (* Cf RamenTyping: if x is a vector and n a constant, then nullability
+     * is that of items or vector: *)
     (match e.E.typ.structure with
-    | TVec (_, t) | TList t ->
+    | TVec (_, t) when E.is_const n ->
         let func = "(fun a_ n_ -> Array.get a_ (Int32.to_int n_))" in
         emit_functionN ~env ~opc ~nullable func
           ~impl_return_nullable:t.nullable
           [None, PropagateNull; Some TI32, PropagateNull] oc [e; n]
+    (* Otherwise the resul is nullable: *)
+    | TVec _ | TList _ ->
+        let func = "(fun a_ n_ -> try NotNull (Array.get a_ (Int32.to_int n_)) \
+                                  with Invalid_argument _ -> Null)" in
+        emit_functionN ~env ~opc ~nullable func
+          ~impl_return_nullable:true
+          [None, PropagateNull; Some TI32, PropagateNull] oc [e; n]
+    (* Never nullable: *)
     | TTuple ts ->
         let n = E.int_of_const n |>
                 option_get "Get from tuple must have const index" __LOC__ in

commit 59a51808519006e6cfa20716f09d853a85b1eee3

Fix: Compilation of Get for lists of nullable items
Regression since dfce37536cc49a90e6e64774cf7103a9fa955319

src/CodeGen_OCaml.ml
@@ -1312,10 +1312,13 @@ and emit_expr_ ~env ~context ~opc oc expr =
         emit_functionN ~env ~opc ~nullable func
           ~impl_return_nullable:t.nullable
           [None, PropagateNull; Some TI32, PropagateNull] oc [e; n]
-    (* Otherwise the resul is nullable: *)
-    | TVec _ | TList _ ->
-        let func = "(fun a_ n_ -> try NotNull (Array.get a_ (Int32.to_int n_)) \
-                                  with Invalid_argument _ -> Null)" in
+    (* Otherwise the result is nullable: *)
+    | TVec (_, t) | TList t ->
+        let func =
+          "(fun a_ n_ -> try " ^
+          (* Make the item nullable if they are not already: *)
+          (if t.T.nullable then "" else "NotNull ") ^
+          "(Array.get a_ (Int32.to_int n_)) with Invalid_argument _ -> Null)" in
         emit_functionN ~env ~opc ~nullable func
           ~impl_return_nullable:true
           [None, PropagateNull; Some TI32, PropagateNull] oc [e; n]