f48cc3fa3a50f347a3a2018b06db5cb8f29d95a0
Damien Bihel <dbihel@accedian.com>
2020-03-13 11:45:42 +0100

    cli: can run ramen with only identity option
    Allow users to run ramen with identiy option only without providing
    priv-key or pub-key options that are redundant.
    
    closes #1150

--- src/RamenCliCmd.ml
+++ src/RamenCliCmd.ml
@@ -66,14 +66,14 @@ let make_copts
   let forced_variants = list_of_string_opt forced_variants
   and masters =
     list_of_string_opt masters |> List.map N.site |> Set.of_list in
-  if srv_pub_key <> "" && (clt_pub_key = "" || clt_priv_key = "") then
-    failwith "To connect securely to that server, the client \
-              private and secret keys must also be provided" ;
   let conf =
     C.make_conf
       ~debug ~quiet ~keep_temp_files ~reuse_prev_files ~forced_variants
       ~initial_export_duration ~site ~bundle_dir ~masters ~sync_url
       ~srv_pub_key ~username ~clt_pub_key ~clt_priv_key ~identity persist_dir in
+  if srv_pub_key <> "" && (conf.clt_pub_key = "" || conf.clt_priv_key = "") then
+    failwith "To connect securely to that server, the client \
+              private and secret keys must also be provided" ;
   (* Find out the ZMQ URL to reach the conf server: *)
   if conf.sync_url <> "" then conf
   else
