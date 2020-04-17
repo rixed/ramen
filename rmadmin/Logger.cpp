7befe8b1dc8ea3e2550e8218030e4e6d266bdfda
Cedric Cellier <rixed@happyleptic.org>
2020-04-17 14:42:03 +0200

    RmAdmin: better have debug output always directed to stdout

--- rmadmin/Logger.cpp
+++ rmadmin/Logger.cpp
@@ -22,7 +22,8 @@ static void messageHandler(
   QtMsgType type, QMessageLogContext const &, QString const &msg)
 {
   // TODO: race condition here
-  if (type != QtFatalMsg && Menu::loggerWin && Menu::loggerWin->logger) {
+  if (type != QtDebugMsg && type != QtFatalMsg &&
+      Menu::loggerWin && Menu::loggerWin->logger) {
     emit Menu::loggerWin->logger->newMessage(type, msg);
   } else {
     std::cerr << logLevel(type).toStdString() << ": " << msg.toStdString() << std::endl;
