83f3e619ac5050ab0d2fbbb6a7fd1d1ee8bd4127
Damien Bihel <biheldamien@gmail.com>
2020-09-23 17:06:50 +0200

    fix: publish stats correctly when a worker ends.
    First we update the stats in the last report.
    Then we wait publish thread before closing a worker.

--- src/CodeGenLib_Skeletons.ml
+++ src/CodeGenLib_Skeletons.ml
@@ -226,14 +226,17 @@ let worker_start conf get_binocle_tuple
   let last_report () =
     (* Sending stats for one last time: *)
     let now = Unix.gettimeofday () in
+    Stats.update () ;
     may_publish_stats conf ~force:true publish_stats now in
   match k publish_stats outputer with
   | exception e ->
       print_exception ~what:"Worker process" e ;
       last_report () ;
+      Option.may (Thread.join) (!Publish.thd) ;
       exit ExitCodes.uncaught_exception
   | () ->
       last_report () ;
+      Option.may (Thread.join) (!Publish.thd) ;
       exit (!quit |? ExitCodes.terminated)
 
 (*
