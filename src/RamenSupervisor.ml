c7f896d2769b6cfff7275eb74a3c9991dc78beaa
Cedric Cellier <rixed@happyleptic.org>
2021-02-18 19:21:59 +0100

    Fix supervisor crash because of watchdog on load
    When too many workers are running supervisor always have a message to
    read and never return to its main loop, thus being killed by the
    watchdog.

--- src/RamenSupervisor.ml
+++ src/RamenSupervisor.ml
@@ -983,7 +983,8 @@ let synchronize_running ?(while_=always) conf kill_at_exit =
     let last_sync = ref 0. in
     while while_ () do
       Option.may Watchdog.reset !watchdog ;
-      ZMQClient.process_in ~while_ session ;
+      let max_count = 30 in (* Need to reset the watchdog from time to time *)
+      ZMQClient.process_in ~while_ ~max_count session ;
       let now = Unix.gettimeofday () in
       if now > !last_sync +. delay_between_worker_syncs then (
         last_sync := now ;
