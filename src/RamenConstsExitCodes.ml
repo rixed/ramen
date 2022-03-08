aeb5c0f73e5ad60aaa98348d2dc9a9bc4e001adb
Cedric Cellier <rixed@happyleptic.org>
2022-03-08 11:51:47 +0100

    Make C-c appear as a success from the shell to prevent restarts

--- src/RamenConstsExitCodes.ml
+++ src/RamenConstsExitCodes.ml
@@ -1,6 +1,6 @@
 (* These first 3 are not us to define: *)
 let terminated = 0
-let interrupted = 1
+let interrupted = 0 (* C-c must not appear as a failure to the shell *)
 let test_failed = 1
 let lwt_uncaught_exception = 2
 let cannot_parse_param = 3
@@ -12,8 +12,8 @@ let confserver_migrated = 8
 let other_error = 9
 
 let string_of_code = function
-  | 0 -> "terminated"
-  | 1 -> "interrupted/failure"
+  | 0 -> "terminated/interrupted"
+  | 1 -> "failure"
   | 2 -> "uncaught exception"
   | 3 -> "cannot parse a parameter"
   | 4 -> "killed by watchdog"
